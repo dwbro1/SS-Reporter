646d2d8642d9fe8087d0cdc832a69996184e8dcf
D.W <dale@tfimed.com>
2016-12-09 22:27:56 -0600

    Update and rename envriophatSensor.py to envirophatSensor.py

---
+++ envirophatSensor.py
@@ -0,0 +1,33 @@
+"""
+   Copyright 2016 Dale Brown
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+ Script: envirophatSensor.py
+ Author: Dale Brown
+ Date:   December 9, 2016
+ Purpose: Checks envirophat sensors and publishes any changes
+
+ TODO: Take advantage of GPIO.add_event_detect(pin, GPIO.RISING, callback=myFunction) 
+       to register for events instead of polling. Use Dash sensor as an example.
+"""
+
+import sys
+from envirophat import light, weather, leds
+
+class envirophatSensor:
+    """Represents a sensors connected"""
+      
+    def publishState(self):
+        """Publishes the current state"""
+        self.publish('weather.temperature()', self.destination)
